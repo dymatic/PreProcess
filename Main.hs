97be4204e6e9b45e1474cc56eb3460dbe223a480
Norton Jenkins <dominantshoe@gmail.com>
2013-04-21 18:33:32 -0400

    Fixed a broken path in Main.hs

--- Main.hs
+++ Main.hs
@@ -6,7 +6,7 @@ import LibHaskell.LibLists
 main = do
 	(b:c:_) <- getArgs
 	code <- openFile b ReadMode
-	langdefines <- openFile "/etc/PreProcess/langdefines" ReadMode
+	langdefines <- openFile "/etc/preprocess/langdefines" ReadMode
 	langDefinesText <- hGetContents langdefines
 	codeText <- hGetContents code
 	let translatedFile = (replList (lines codeText) (makeAllParameters (sanitize (lines langDefinesText)))) in writeFile c $ unwords $ map ((flip appendList) "\n") translatedFile 
